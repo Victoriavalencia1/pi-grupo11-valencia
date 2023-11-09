9b88b9b4d1b71628c87359fc508710d832518ef1
Victoriavalencia1 <mvalencia@udesa.edu.ar>
2023-11-09 19:23:20 -0300

    armando los endpoints en el js

---
+++ js/home.js
@@ -0,0 +1,13 @@
+const cargarPeliculas = () {
+    fetch(url)
+.then(function(response) {
+    return response.json()
+})
+.then(function(data) {
+    console.log(data);
+})
+.catch(function(error) {
+    console.log(error);
+});
+}
+
