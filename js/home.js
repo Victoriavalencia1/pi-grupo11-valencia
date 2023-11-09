d3ca4ff2b0429f88956194d351f26bcde7030043
Victoriavalencia1 <mvalencia@udesa.edu.ar>
2023-11-09 19:50:29 -0300

    arrancando por los fetch

--- js/home.js
+++ js/home.js
@@ -1,13 +1,14 @@
-const cargarPeliculas = () {
-    fetch(url)
+let endpoint= "https://api.themoviedb.org/3/movie/popular?api_key=99871c7c00dfc64424c61b446dd23039";
+
+fetch(endpoint)
 .then(function(response) {
     return response.json()
 })
 .then(function(data) {
-    console.log(data);
+    console.log(data.results);
 })
 .catch(function(error) {
     console.log(error);
 });
-}
+
 
