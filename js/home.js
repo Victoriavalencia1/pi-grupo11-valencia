2c8e015d3b05e007ea1d835250616eed650cf06b
Victoriavalencia1 <mvalencia@udesa.edu.ar>
2023-11-10 16:09:26 -0300

    trabaje sobre el home js

--- js/home.js
+++ js/home.js
@@ -1,53 +1,76 @@
 let acaVaLaAPIKey = "99871c7c00dfc64424c61b446dd23039";
 
-let pagina= 1;
-const btnAnterior = document.getElementById('btnAnterior');
-const btnSiguiente = document.getElementById('btnSiguiente');
+let urlPelisPopu= `https://api.themoviedb.org/3/movie/popular?api_key=${acaVaLaAPIKey}`;
+let urlSeriesPopu= `https://api.themoviedb.org/3/tv/popular?api_key=${acaVaLaAPIKey}`;
 
-btnSiguiente.addEventListener('click', function(){
-    if (pagina<1000){
-        pagina += 1;
-        allPeliculas();
-    }
-});
 
-btnAnterior.addEventListener('click', function(){
-    if (pagina >1){
-        pagina -= 1;
-        allPeliculas();
-    }
-});
 
-let endpoint= `https://api.themoviedb.org/3/movie/popular?api_key=99871c7c00dfc64424c61b446dd23039&page=${acaVaLaAPIKey}`;
+let contenedorPelisPopular= document.querySelector(".contenedorpeliculas");
+let contenedorSeriesPopular= document.querySelector(".contenedorpeliculas1");
 
-fetch(endpoint)
+
+fetch(urlPelisPopu)
 .then(function(response) {
     return response.json()
 })
 .then(function(data) {
-    console.log(data.results);
 
-    let peliculas = data.results;
+    let miData = data.results;
 
-    let seccion= document.querySelector(".contenedorpeliculas");
+    let contenido= "";
 
-    let allPeliculas= "";
+    for (let i = 0; i <8; i++) {
 
-    for (let i = 0; i < peliculas.length; i++) {
-        let titulo = peliculas[i].title;
-        let imagenURL = "https://image.tmdb.org/t/p/w500" + peliculas[i].poster_path;
-  
-        allPeliculas += `
-          <div>
-            <h2>${titulo}</h2>
-            <a href="./detalle-Pelicula.html?idPelicula=${peliculas[i].id}">
-              <img class="fotopelicula" src="${imagenURL}">
-            </a>
-          </div>`;
-      }
-      seccion.innerHTML = allPeliculas;
+        console.log(miData[i]);
+        contenido += `<article>
+                        <h2>${miData[i].title}</h2>
+                        <a href="./detalle-Pelicula.html?idPelicula=${miData[i].id}"> 
+                        <img class="fotopelicula" src="https://image.tmdb.org/t/p/w500/${miData[i].poster_path}"> 
+                        </a>
+                        <h6>${miData[i].release_date}</h6>
+                    </article>`
+    }
+       
+    contenedorPelisPopular.innerHTML = contenido;
 
 })
+
 .catch(function(error) {
     console.log(error);
 });
+
+
+
+
+/* segundo fetch*/
+
+fetch(urlSeriesPopu)
+.then(function(response) {
+    return response.json()
+})
+.then(function(data) {
+
+    let miData = data.results;
+
+    let contenido= "";
+
+    for (let i = 0; i <8; i++) {
+
+        console.log(miData[i]);
+        contenido += `<article>
+                        <h2>${miData[i].title}</h2>
+                        <a href="./detalle-Pelicula.html?idPelicula=${miData[i].id}"> 
+                        <img class="fotopelicula" src="https://image.tmdb.org/t/p/w500/${miData[i].poster_path}"> 
+                        </a>
+                        <h6>${miData[i].release_date}</h6>
+                    </article>`
+    }
+       
+    contenedorSeriesPopular.innerHTML = contenido;
+
+})
+
+.catch(function(error) {
+    console.log(error);
+});
+
