eb2a9969bdd2bf93ce2f6c5a57d7f7dae70d76c6
clemenrosella <rosellac@udesa.edu.ar>
2023-11-10 16:29:47 -0300

    Edite busqueda.js y cambie el url

--- js/busqueda.js
+++ js/busqueda.js
@@ -2,5 +2,38 @@ let qs = location.search;
 let qsObj= new URLSearchParams(qs);
 let nombrePelicula= qsObj.get("nombrePersonaje");
 
-let url= `https://api.themoviedb.org/3/movie/popular?name=${nombrePelicula}`
+let url= `https://api.themoviedb.org/3/search/movie?api_key=${acaVaLaAPIKey}&query=${busqueda}`
 
+fetch(url)
+.then(function(res) {
+
+    return res.json();
+
+})
+.then(function(data)  {
+
+    console.log(data.results);
+
+    let personajes = data.results;
+    let seccion = document.querySelector(".container")
+    let miPersonaje = document.querySelector(".miPersonaje")
+
+    let allCharacters = "";
+
+    for (let i = 0; i < personajes.length; i++) {
+        allCharacters += `<article>
+        <h2>${miData[i].title}</h2>
+        <a href="./detalle-Pelicula.html?idPelicula=${miData[i].id}"> 
+        <img class="fotopelicula" src="https://image.tmdb.org/t/p/w500/${miData[i].poster_path}"> 
+        </a>
+        <h6>${miData[i].release_date}</h6>
+    </article>`
+    }
+
+    seccion.innerHTML = allCharacters;
+    miPersonaje.innerText = nombrePersonaje;
+
+})
+.catch(function(error)  {
+    console.log(error);
+}) 
