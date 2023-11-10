9a279f7f54c4817e5042a626bcf0e887d35ff829
Victoriavalencia1 <mvalencia@udesa.edu.ar>
2023-11-09 21:08:29 -0300

    cambiando el home js

--- js/home.js
+++ js/home.js
@@ -6,9 +6,28 @@ fetch(endpoint)
 })
 .then(function(data) {
     console.log(data.results);
+
+    let peliculas = data.results;
+
+    let seccion= document.querySelector(".contenedorpeliculas");
+
+    let allPeliculas= "";
+
+    for (let i = 0; i < peliculas.length; i++) {
+        let titulo = peliculas[i].title;
+        let imagenURL = "https://image.tmdb.org/t/p/w500" + peliculas[i].poster_path;
+  
+        allPeliculas += `
+          <div>
+            <h2>${titulo}</h2>
+            <a href="./detalle-Pelicula.html">
+              <img class="fotopelicula" src="${imagenURL}">
+            </a>
+          </div>`;
+      }
+      seccion.innerHTML = allPeliculas;
+
 })
 .catch(function(error) {
     console.log(error);
 });
-
-
