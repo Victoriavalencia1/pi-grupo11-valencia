206c556e97e49412e1d5a5cd5f802043d9808807
Victoriavalencia1 <mvalencia@udesa.edu.ar>
2023-11-11 12:08:00 -0300

    cree el buscador de peliculas, y complete el fetch de busqueda js

--- js/busqueda.js
+++ js/busqueda.js
@@ -1,39 +1,40 @@
+let acaVaLaAPIKey = "99871c7c00dfc64424c61b446dd23039";
+
 let qs = location.search;
 let qsObj= new URLSearchParams(qs);
 let nombrePelicula= qsObj.get("NombrePelicula");
 
-let url= `https://api.themoviedb.org/3/search/movie?api_key=${acaVaLaAPIKey}&query=${nombrePelicula}`
+let url= `https://api.themoviedb.org/3/search/movie?api_key=${acaVaLaAPIKey}&query=${nombrePelicula}`;
+
+let contenedorBuscador= document.querySelector(".contenedorpeliculas");
+
 
 fetch(url)
-.then(function(res) {
-
-    return res.json();
-
+.then(function(response) {
+    return response.json()
 })
-.then(function(data)  {
+.then(function(data) {
 
-    console.log(data.results);
+    let miData = data.results;
 
-    let personajes = data.results;
-    let seccion = document.querySelector(".container")
-    let miPersonaje = document.querySelector(".miPersonaje")
+    let contenido= "";
 
-    let allCharacters = "";
+    for (let i = 0; i <8; i++) {
 
-    for (let i = 0; i < personajes.length; i++) {
-        allCharacters += `<article>
-                            <h2>${miData[i].title}</h2>
-                            <a href="./detalle-Pelicula.html?idPelicula=${miData[i].id}"> 
-                            <img class="fotopelicula" src="https://image.tmdb.org/t/p/w500/${miData[i].poster_path}"> 
-                            </a>
-                            <h6>${miData[i].release_date}</h6>
-                            </article>`
+        console.log(miData[i]);
+        contenido += `<article>
+                        <h2>${miData[i].title}</h2>
+                        <a href="./detalle-Pelicula.html?idPelicula=${miData[i].id}"> 
+                        <img class="fotopelicula" src="https://image.tmdb.org/t/p/w500/${miData[i].poster_path}"> 
+                        </a>
+                        <h6>${miData[i].release_date}</h6>
+                    </article>`
     }
-
-    seccion.innerHTML = allCharacters;
-    miPersonaje.innerText = nombrePersonaje;
+       
+    contenedorBuscador.innerHTML = contenido;
 
 })
-.catch(function(error)  {
+
+.catch(function(error) {
     console.log(error);
-}) 
+});
