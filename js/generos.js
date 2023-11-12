1d40e50e54fd9b24d5a2a529fb7cd53a4409ab91
inakiga <143835247+inakiga@users.noreply.github.com>
2023-11-12 14:54:49 -0300

    modifique el js de generos

--- js/generos.js
+++ js/generos.js
@@ -6,4 +6,53 @@ let urlGeneroSerie= `https://api.themoviedb.org/3/genre/tv/list?api_key=${acaVaL
 
 let contenedorGenerosPeli = document.querySelector("#contenedorPeliGenero");
 let contenedorGenerosSerie= document.querySelector("#contenedorSerieGenero");
+fetch(urlGeneroPeli)
+.then(function(response) {
+    return response.json()
+})
+.then(function(data) {
 
+    let miData = data.genres;
+
+    let contenido= "";
+
+    for (let i = 0; i < miData.length; i++) {
+
+        console.log(miData[i]);
+        contenido += `<li class="cajagenero"> 
+                        <a href="./detalle-Genero.html">${miData[i].name}</a> 
+                    </li>`
+    }
+       
+    contenedorGenerosPeli.innerHTML = contenido;
+
+})
+.catch(function(error) {
+    console.log(error);
+});
+/segundo fetch/
+
+fetch(urlGeneroSerie)
+.then(function(response) {
+    return response.json()
+})
+.then(function(data) {
+
+    let miData = data.genres;
+
+    let contenido= "";
+
+    for (let i = 0; i < miData.length; i++) {
+
+        console.log(miData[i]);
+        contenido += `<li class="cajagenero"> 
+                        <a href="./detalle-Genero.html">${miData[i].name}</a> 
+                    </li>`
+    }
+       
+    contenedorGenerosSerie.innerHTML= contenido;
+
+})
+.catch(function(error) {
+    console.log(error);
+});
