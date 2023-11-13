bf7bd8765ac07beff1a2f00552ba07a0778386e7
Victoriavalencia1 <mvalencia@udesa.edu.ar>
2023-11-13 12:12:38 -0300

    sigo modificando el detalle serie

--- js/detalle-serie.js
+++ js/detalle-serie.js
@@ -25,7 +25,7 @@ fetch(urlDetalleSerie)
     titulo.innerText= data.name;
     calificacion.innerText = `Popularidad: ${data.popularity}`;
     fechaEstreno.innerText = `Fecha de estreno: ${data.first_air_date}`;
-    sinopsis.innerText= data.overview;
+    sinopsis.innerText= `Sinopsis: ${data.overview}`;
     let generos = data.genres.map((genre) => genre.name);
     genero.innerText = `${generos.join(", ")}`;
 
@@ -34,5 +34,51 @@ fetch(urlDetalleSerie)
 .catch(function(error) {
     console.log(error);
 });
+
+
+/*recomendaciones*/
+var urlParams = new URLSearchParams(window.location.search);
+    var query = urlParams.get("idSerie");
+    var url = `https://api.themoviedb.org/3/movie/"+ ${query} + "/recommendations?api_key=${acaVaLaAPIKey}&language=en-US&page=1`
+    
+    fetch(url)
+      .then(function(respond) {
+        return respond.json();
+      })
+    
+      .then(function(data) {
+        console.log(data);
+    
+        var ul = document.querySelector("ul.recomen")
+    
+        for (var i = 0; i < data.results.length; i++) {
+          var id = data.results[i].id;
+          var title = data.results[i].title;
+          var foto = data.results[i].poster_path;
+    
+          li = "<li class='li'>"
+            li += "<p class='titulos'>"+ title + "</p>"
+            li += "<a href='detalle-Pelicula.html?idSerie=" + id + "'>"
+            li += "<img class=imgPelis src=https://image.tmdb.org/t/p/w500/"+ foto + ">"
+            li += "</a>"
+          li += "</li>"
+    
+          ul.innerHTML += li
+    
+        }
+    
+      })
+    
+      .catch(function(error) {
+        return console.log("Error" + error);
+      })
+    
+    // Que se abran las recomendaciones
+      var recom = document.querySelector ('#ver-recomendaciones')
+      recom.addEventListener ("click", function() {
+        // cuando clickeas en el boton se abren las recomendaciones
+            var ul = document.querySelector("ul.recomen")
+            ul.classList.toggle("display-none")
+    })
 
 
