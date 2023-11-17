5081d207dfd82b4fccc8af5c5188921656214b6f
clemenrosella <rosellac@udesa.edu.ar>
2023-11-17 10:38:27 -0300

    Operando y probando con favoritos js

--- js/favoritos.js
+++ js/favoritos.js
@@ -2,7 +2,6 @@ let acaVaLaAPIKey = '99871c7c00dfc64424c61b446dd23039';
 
 
 
-
 /*Boton favoritos*/
     
 const botonFavoritos = document.getElementById('buscador');
@@ -41,3 +40,51 @@ botonFavoritos.addEventListener('click', function () {
         botonFavoritos.textContent = 'Añadir a favoritos';
     }
 });
+
+for (let i = 0; i < favs.length; i++) {
+    const element = favs[i];  
+    console.log(element)
+     
+
+      if (element.tipo == "movie") {
+
+        fetch(`https://api.themoviedb.org/3/movie/${element.id}?api_key=${apiKey}&language=en-US`)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (Object) {
+            preferidos.innerHTML += 
+            `
+            <article>
+            <a href="movies_detail.html?tipo=movie&id=${Object.id}"><img src="${imgURL}${Object.poster_path}" alt=""></a>
+          </article>
+            `
+        })
+    
+        .catch(function (error) {
+            console.log(`el error fue ${error}`)
+        })
+    
+        
+    } else {
+    
+        fetch(`https://api.themoviedb.org/3/tv/${element.id}?api_key=${apiKey}&language=en-US`)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (Object) {
+            preferidos.innerHTML += 
+            `
+            <article>
+            <a href="movies_detail.html?tipo=tv&id=${Object.id}"><img src="${imgURL}${Object.poster_path}" alt=""></a>
+          </article>
+            `
+        })
+    
+        .catch(function (error) {
+            console.log(`el error fue ${error}`)
+        })
+    
+    }
+    
+  }
